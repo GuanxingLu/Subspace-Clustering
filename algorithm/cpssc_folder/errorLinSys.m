1cdf10ad3fdde1859297c411a1ea349942c5e0b1
GuanxingLu <70553669+GuanxingLu@users.noreply.github.com>
2022-02-19 18:27:53 +0800

    Delete algorithm/cpssc_folder directory

--- algorithm/cpssc_folder/errorLinSys.m
+++
@@ -1,4 +0,0 @@
-function err = errorLinSys(X, J)
-chg = abs(X-X*J);
-err = max(chg(:));
-end
--- algorithm/cpssc_folder/lansvd.m
+++
@@ -1,6 +0,0 @@
-function spectralNorm = lansvd(B)
-
-[~,S,~] = svd(B'*B,'econ');
-spectralNorm = sqrt(max(S));
-
-end
