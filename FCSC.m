5ffe319f7de32c8dafe809ddf05cdfe4094d5b14
lg(x) <70553669+GuanxingLu@users.noreply.github.com>
2022-11-03 14:11:17 +0800

    Update FCSC.m

--- FCSC.m
+++ FCSC.m
@@ -5,11 +5,10 @@ W = zeros(m,n);
 for i = 1:m
     for j = 1:n
         if B(i, j)>=0
-            W(i,j) = 1 - (1-B(i,j))*(1 - Z(i,j));   %若B是1，则该项是1
+            W(i,j) = 1 - (1-B(i,j))*(1 - Z(i,j));
         else
-            W(i,j) = (1+ B(i, j))*Z(i,j);   %若B是-1，则该项是0
+            W(i,j) = (1+ B(i, j))*Z(i,j);
         end
     end
 end
-    
 end
